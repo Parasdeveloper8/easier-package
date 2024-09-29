f39f4170bf80006dc795a641014eddc4308af7c2
Paras prajapat <parasprajapat8th.gmail.com>
2024-09-30 00:54:18 +0530

    New methods + readme updated

---
+++ package/README.md
@@ -0,0 +1,43 @@
+#Built with javascript for easy javascipt
+Use many methods of console in short
+___________________________________________________________
+*author:-paras prajapat*
+**github:-parasdeveloper8**
+
+methods available :----------------------------------------------------------------
+
+1.clog("This is a log message.");
+
+2.cerr("This is an error message.");
+
+3.cwarn("This is a warning message.");
+
+4.cinfo("This is an info message.");
+
+5.cbug("This is a debug message.");
+
+6.ctrace("This is a trace message.");
+
+##const sampleArray = [{ name: "Alice", age: 25 }, { name: "Bob", age: 30 }];
+7.ctbl(sampleArray);
+
+8.cclr():-console.clear();
+
+9.hlf():-retruns half
+ 
+10.binarytochar() :-binary to character
+   
+11.raise(base,exponent):- Math.pow(base,exponent);
+
+12.mult(a,b) :- a x b
+
+13.tpm(a,b,c) :- perimeter of triangle
+
+14.rpm(a,b,c,d) :- perimeter of rectangle
+
+15.cpm(radius) :- circumference of circle
+
+16.scpm(radius) :- circumference of semi-circle
+
+17.table(num) :- writes table of given argument
+
--- package/index.js
+++ package/index.js
@@ -11,4 +11,52 @@ function sqr(num) {
     }
     return Math.sqrt(num);
 }
-module.exports = {clog,cerr,sqr}
+function cwarn(txt){
+    console.warn(txt);
+}
+function cinfo(txt){
+    console.info(txt);
+}
+function cbug(txt){
+    console.debug(txt);
+}
+function ctrace(txt){
+    console.trace(txt);
+}
+function ctbl(arr){
+    console.table(arr);
+}
+function cclr(){
+    console.clear();
+}
+function hlf(numb){
+    return numb/2;
+}
+function binarytochar(binary) {
+    return binary.split(' ')
+        .map(b => String.fromCharCode(parseInt(b, 2)))
+        .join('');
+}
+
+function raise(base,exponent){
+     return Math.pow(base,exponent);
+}
+function mult(a,b){
+     return a*b;
+}
+function tpm(a,b,c){
+    return a + b + c;
+}
+function rpm(a,b,c,d){
+    return a + b + c + d;
+}
+function cpm(radius){
+    return 2*Math.PI*radius;
+}
+function scpm(radius){
+    return Math.PI*radius**2;
+}
+function table(num){
+    return [num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9,num*10,];
+}
+module.exports = {clog,cerr,sqr,cwarn,cinfo,cbug,ctrace,ctbl,cclr,hlf,binarytochar,raise,rpm,cpm,scpm,table,tpm,mult}
